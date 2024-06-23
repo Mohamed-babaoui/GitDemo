5e47a4e7800f3989fc73b60b10d1e0d67d01087a
Mohamed <babaouimohamedse@gmail.com>
2024-06-23 16:46:42 +0100

    After gitX guy edits

--- src/test/java/TestRunner/Runner.java
+++ src/test/java/TestRunner/Runner.java
@@ -4,5 +4,10 @@ package TestRunner;
 public class Runner {
     public static void main(String[] args) {
         System.out.println(" this was added now and deleted what whas previously");
+        function();
+    }
+   public static void function()
+    {
+        System.out.println("i am an added function");
     }
 }
---
+++ src/test/java/TestRunner/Runner2.java
@@ -0,0 +1,13 @@
+package TestRunner;
+
+
+public class Runner2 {
+    public static void main(String[] args) {
+        System.out.println(" this is class 2");
+        function();
+    }
+   public static void function()
+    {
+        System.out.println("i am an added function from class 2 ");
+    }
+}
